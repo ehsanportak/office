4caaf93471c7a84340a3519449214284fe2fb45d
ehsanportak <ehsanportak@gmail.com>
2020-01-25 11:35:23 +0330

    ok

---
+++ wp-content/plugins/ep_metabox_test/admin/view/ep_admin_view.php
@@ -0,0 +1,7 @@
+
+<?php
+update_post_meta('25','_epmb_genres',sanitize_text_field($_POST['epmb_genres']));
+?>
+درام<input type="radio" id="epmb_genres" name="epmb_genres" value="1"><br>
+جنایی<input type="radio" id="epmb_genres" name="epmb_genres" value="2"><br>
+احساسی<input type="radio" id="epmb_genres" name="epmb_genres" value="3">
